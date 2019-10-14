5418ce2e43b2be9efc694fea0afed2297310a9e9
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-10-14 23:17:35 +0300

    Modified

--- src/redux/reducers/AuthReducer.js
+++ src/redux/reducers/AuthReducer.js
@@ -1,19 +1,32 @@
 import { SET_CURRENT_USER } from "../actions/actionTypes";
 
-const initialState = {
-  user: null
-};
+const initialState = null;
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
-      return {
-        ...state,
-        user: action.payload
-      };
+      return action.payload;
 
     default:
       return state;
   }
 };
 export default reducer;
+
+// export default setCurrentUser = token => {
+//   let user;
+//   if (token) {
+//     localStorage.setItem("token", token);
+//     axios.defaults.headers.common.Authorization = `jwt ${token}`;
+//     user = jwt_decode(token);
+//   } else {
+//     localStorage.removeItem("token");
+//     delete axios.defaults.headers.common.Authorization;
+//     user = null;
+//   }
+
+//   return {
+//     type: SET_CURRENT_USER,
+//     payload: user
+//   };
+// };
