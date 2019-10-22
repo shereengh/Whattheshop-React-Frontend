72b6e7265ab6b12cf7c7411160c2f5c83832ff2c
shereengh <shereengaa@gmail.com>
2019-10-22 10:54:47 +0300

    search bar functioning

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 class Loading extends Component {
   render() {
     return (
       <div className="spinner mx-auto text-center">
-        {/* <FontAwesomeIcon icon={faSpinner} spin size="4x" /> */}
+        <FontAwesomeIcon icon={faSpinner} spin size="4x" />
       </div>
     );
   }
--- src/MealsList.js
+++ src/MealsList.js
@@ -2,19 +2,39 @@ import React, { Component } from "react";
 
 // Components
 import MealCard from "./MealCard";
+import SearchBar from "./SearchBar";
 
 import { connect } from "react-redux";
 import * as actionCreators from "./redux/actions/index";
 
 class MealsList extends Component {
+  state = {
+    filteredMeals: []
+  };
+
+  componentDidMount = async () => {
+    await this.props.fetchMeals();
+    this.setState({
+      filteredMeals: this.props.meals
+    });
+  };
+
+  filterMeals = query => {
+    query = query.toLowerCase();
+    let filteredMeals = this.props.meals.filter(meal => {
+      return `${meal.name}`.toLowerCase().includes(query.toLowerCase());
+    });
+    this.setState({ filteredMeals: filteredMeals });
+  };
   render() {
-    const mealCards = this.props.meals.map(meal => (
+    const mealCards = this.state.filteredMeals.map(meal => (
       <MealCard key={meal.id} meal={meal} />
     ));
 
     return (
       <div>
         <h3>Available Meals</h3>
+        <SearchBar filter={this.filterMeals} />
         <div className="row">{mealCards}</div>
       </div>
     );
